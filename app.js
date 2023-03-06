30393682d6eedbcb4f6a1a65d9b2a1e11e5fa6f1
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-03-06 13:02:28 +0530

    create http

--- app.js
+++ app.js
@@ -15,3 +15,16 @@ http.createServer(function (req, res) {
  res.write('Hello World!');
  res.end();
 }).listen(8080);
+
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+ let data = '';
+ resp.on('data', (chunk) => {
+ data += chunk;
+ });
+ resp.on('end', () => {
+ console.log(JSON.parse(data));
+ });
+}).on('error', (err) => {
+ console.log("Error: " + err.message);
+});
