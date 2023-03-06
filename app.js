51c1cf0d67da9ff9527cb12d22c91b8a8cafa32a
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-03-06 12:58:15 +0530

    create webserver

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
  if (err) throw err;
  console.log('File saved!');
 });
+
+const http = require('http');
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello World!');
+ res.end();
+}).listen(8080);
